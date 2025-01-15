8c7608148849f515a18383747f9c2f29064df11b
M. Bima Ar Rozaq <rozaqba@gmail.com>
2025-01-16 06:08:42 +0700

    Add files via upload

---
+++ webskafa.github.io-main/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    adaptiveHeight: true,
+    auto: true,
+    item: 1,
+    slideMargin: 0,
+    loop: true,
+  });
+});
+
+const navbarHome = document.querySelector(".navbarHome");
+
+window.addEventListener("scroll", () => {
+  const post = window.scrollY > 100;
+
+  navbarHome.classList.toggle("scroll", post);
+});
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+});
+
